65f720f505293fcaeb533c85fabc83bdc59dd681
fcanata00 <fcannata00@gmail.com>
2025-10-20 18:08:20 -0300

    Delete lfsctl_completion.sh

--- lfsctl_completion.sh
+++
@@ -1,9 +0,0 @@
-_lfsctl_completion() {
-  local cur prev cmds
-  COMPREPLY=()
-  cur="${COMP_WORDS[COMP_CWORD]}"
-  cmds="bootstrap bs build b install i uninstall u update up deps d pipeline info logs help"
-  COMPREPLY=( $(compgen -W "$cmds" -- "$cur") )
-  return 0
-}
-complete -F _lfsctl_completion lfsctl
